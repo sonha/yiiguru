795e94bd5d2328ee9172f3301ed0b11d82559634
sonha <sonha@users.noreply.github.com>
2014-08-05 18:16:01 +0700

    update code

--- editcode/tryit.php
+++ editcode/tryit.php
@@ -1,56 +1,20 @@
-<?php 
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>attributeContainsWord demo</title>
+  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+</head>
+<body>
  
-class MyClass 
-{ 
-    public $prop1 = "I'm a class property!"; 
+<input name="man-news">
+<input name="milk man">
+<input name="letterman2">
+<input name="newmilk">
  
-    public function __construct() 
-    { 
-        echo 'The class "', __CLASS__, '" was initiated!<br />'; 
-    } 
+<script>
+$( "input[name~='man']" ).val( "mr. man is in it!" );
+</script>
  
-    public function __destruct() 
-    { 
-        echo 'The class "', __CLASS__, '" was destroyed.<br />'; 
-    } 
- 
-    public function __toString() 
-    { 
-        echo "Using the toString method: "; 
-        return $this->getProperty(); 
-    } 
- 
-    public function setProperty($newval) 
-    { 
-        $this->prop1 = $newval; 
-    } 
- 
-    public function getProperty() 
-    { 
-        return $this->prop1 . "<br />"; 
-    } 
-} 
- 
-class MyOtherClass extends MyClass 
-{ 
-     public function __construct() 
-    { 
-        echo 'Day la mot ham khoi tao cua "', __CLASS__, '" ham khoi tao khac!<br />'; 
-    } 
-    
-    public function newMethod() 
-    { 
-        echo 'From a new method in "', __CLASS__, '".<br />'; 
-    } 
-} 
- 
-// Tạo đối tượng 
-$newobj = new MyOtherClass; 
- 
-// Echo object ra dạng chuỗi 
-echo $newobj->newMethod(); 
- 
-// Sử dụng một phương thức của class cha 
-echo $newobj->getProperty(); 
- 
-?>
+</body>
+</html>
