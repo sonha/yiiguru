78ae402f92dec243fe49f65ade422778813edcd0
Ha Anh Son <hason61vn@gmail.com>
2014-09-16 11:24:20 +0700

    Upload code demo

--- editcode/tryit.php
+++ editcode/tryit.php
@@ -1,31 +1,21 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <body>
-    <div id="sonha"></div>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-    
-  <script type="text/javascript">
-  $(document).ready(function(){ 
-  $.ajax({
-  url : "http://api.flickr.com/services/feeds/photos_public.gne?tags=resig&tagmode=all&format=json&jsoncallback=?",
-  dataType : "json",
-  success : function(data) {
-  $.each(data.items, function(i, item){
-  console.log(status);
-  $( "<img />" ).attr( {title: function() {
-  return i + " - photo by Kelly Clark";
-}, src: item.media.m}).appendTo("#sonha");
-    // $("<img />").attr("src", item.media.m).appendTo('#sonha');
-    // $("<img />").attr({src: item.media.m, alt : item.title, title : item.title}).appendTo('#sonha');
-  });
-  }, 
-  error: function (data, status) {
-  console.log(status);
-    console.log("In local error callback.");
-  }
 
-  });
-  });
-  </script>
+<p id="demo"></p>
+
+<script>
+var txt = "";
+var person = {fname:"John", lname:"Doe", age:25}; 
+var x;
+for (x in person) {
+    console.log(x);
+    console.log(person[x]);
+    txt += person[x] + " ";
+}
+
+document.getElementById("demo").innerHTML = txt;
+</script>
+
 </body>
 </html>
