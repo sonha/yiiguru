4e411e159e9c0d638cd5c447492cae8c68c01759
Ha Anh Son <hason61vn@gmail.com>
2014-09-16 11:29:20 +0700

    Upload code demo

--- editcode/demo/noname.php
+++ editcode/demo/noname.php
@@ -1,56 +1,26 @@
 <?php 
- 
-class MyClass 
-{ 
-    public $prop1 = "I'm a class property!"; 
- 
-    public function __construct() 
-    { 
-        echo 'The class "', __CLASS__, '" was initiated!<br />'; 
-    } 
- 
-    public function __destruct() 
-    { 
-        echo 'The class "', __CLASS__, '" was destroyed.<br />'; 
-    } 
- 
-    public function __toString() 
-    { 
-        echo "Using the toString method: "; 
-        return $this->getProperty(); 
-    } 
- 
-    public function setProperty($newval) 
-    { 
-        $this->prop1 = $newval; 
-    } 
- 
-    public function getProperty() 
-    { 
-        return $this->prop1 . "<br />"; 
-    } 
-} 
- 
-class MyOtherClass extends MyClass 
-{ 
-     public function __construct() 
-    { 
-        echo 'Day la mot ham khoi tao cua "', __CLASS__, '" ham khoi tao khac!<br />'; 
-    } 
-    
-    public function newMethod() 
-    { 
-        echo 'From a new method in "', __CLASS__, '".<br />'; 
-    } 
-} 
- 
-// Tạo đối tượng 
-$newobj = new MyOtherClass; 
- 
-// Echo object ra dạng chuỗi 
-echo $newobj->newMethod(); 
- 
-// Sử dụng một phương thức của class cha 
-echo $newobj->getProperty(); 
- 
+@echo 1 / 0;
+// generates "Parse error: syntax error, unexpected T_ECHO" since 
+// echo is not an expression
+
+echo @(1 / 0);
+// suppressed "Warning: Division by zero"
+
+@$i / 0;
+// suppressed "Notice: Undefined variable: i"
+// displayed "Warning: Division by zero"
+
+@($i / 0);
+// suppressed "Notice: Undefined variable: i"
+// suppressed "Warning: Division by zero"
+
+$c = @$_POST["a"] + @$_POST["b"];
+// suppressed "Notice: Undefined index: a"
+// suppressed "Notice: Undefined index: b"
+
+$c = @foobar();
+echo "Script was not terminated";
+// suppressed "Fatal error: Call to undefined function foobar()"
+// however, PHP did not "ignore" the error and terminated the
+// script because the error was "fatal"
 ?>
